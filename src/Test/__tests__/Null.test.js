ba40bb60cc4e8a0a6b53d32f21e2953be8fc7185
KarpovNick <karpov.n.g@gmail.com>
2019-06-14 22:38:50 +0300

    install jest enviroument
    Hi from the template commit-msg hook

---
+++ src/Test/__tests__/Null.test.js
@@ -0,0 +1,12 @@
+
+test('null', () => {
+  const n = null;
+
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+
+});
+
